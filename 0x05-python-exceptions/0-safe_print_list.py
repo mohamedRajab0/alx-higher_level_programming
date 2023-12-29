7867de90e29e6c770a1221a8ab3faf4096e243cf
mohamedRajab0 <elzaimmohamed@gmail.com>
2023-12-29 06:22:42 -0800

    upload

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/user/bin/python3
+def safe_print_list(my_list=[], x=0):
+    i = 0
+    try:
+        while i > x:
+            print(my_list[i], end='')
+            i++
+    except IndexError:
+        None
+    print()
+    return i
