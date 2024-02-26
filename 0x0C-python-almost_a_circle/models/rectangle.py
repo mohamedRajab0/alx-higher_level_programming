bf862bb87206ffadffb2a7c2396fdf7ad40ddcf1
mohamedRajab0 <elzaimmohamed@gmail.com>
2024-02-26 21:08:44 +0200

    done

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -2,6 +2,7 @@
 """ Module Rectangle class """
 
 from .base import Base
+import json
 
 
 class Rectangle(Base):
@@ -16,22 +17,20 @@ class Rectangle(Base):
             x (int): The x coordinate of the rectangle
             y (int): The y coordinate of the rectangle
         """
+        super().__init__(id)
         self.width = width
         self.height = height
         self.x = x
         self.y = y
-        super().__init__(id)
 
     @property
     def width(self):
         """Retrieves the width attribute."""
-
         return self.__width
 
     @width.setter
     def width(self, value):
         """Sets the width attribute."""
-
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value <= 0:
@@ -41,13 +40,11 @@ class Rectangle(Base):
     @property
     def height(self):
         """Retrieves the height attribute."""
-
         return self.__height
 
     @height.setter
     def height(self, value):
         """Sets the height attribute."""
-
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value <= 0:
@@ -57,7 +54,6 @@ class Rectangle(Base):
     @property
     def x(self):
         """Retrieves the x attribute."""
-
         return self.__x
 
     @x.setter
@@ -72,13 +68,11 @@ class Rectangle(Base):
     @property
     def y(self):
         """Retrieves the y attribute."""
-
         return self.__y
 
     @y.setter
     def y(self, value):
         """Sets the y attribute."""
-
         if not isinstance(value, int):
             raise TypeError("y must be an integer")
         if value < 0:
@@ -86,61 +80,41 @@ class Rectangle(Base):
         self.__y = value
 
     def area(self):
-         """Calculates the area of a Rectangle instance.
-        Returns: area
-        """
-
+        """Calculates the area of the rectangle"""
         return self.width * self.height
 
     def display(self):
         """Prints the Rectangle instance with the # character."""
-
         print("\n" * self.y, end="")
         for v in range(self.height):
-            print (" " * self.x, end ="")
+            print(" " * self.x, end="")
             for h in range(self.width):
                 print("#", end="")
             print()
 
     def __str__(self):
         """Returns a string representation of a Rectangle instance."""
-
-        return f"[rectangle] ({self.id}) {self.x}/{self.y} - {self.width}/{self.height}"
+        return f"[Rectangle] ({self.id}) {self.x}/{self.y} - {self.width}/{self.height}"
 
     def update(self, *args, **kwargs):
-         """Updates attributes of an instance.
-        Args:
-            - id attribute
-            - width attribute
-            - height attribute
-            - x attribute
-            - y attribute
-        """
-
+        """Updates attributes of an instance."""
         if args:
-            attr = ['id', 'x', 'y', 'width', 'height']
+            attributes = ['id', 'width', 'height', 'x', 'y']
             for i, value in enumerate(args):
-                setattr(self, attr[i], value)
+                setattr(self, attributes[i], value)
         else:
             for key, value in kwargs.items():
                 setattr(self, key, value)
 
-        def save_to_file(cls, list_objs):
-            """"define save to file function
-            Args:
-                list_objs: list which will save to file
-                """
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Writes the JSON string representation of list_objs to a file."""
+        filename = cls.__name__ + ".json"
+        if list_objs is None:
             list_objs = []
-            filename = cls.__name__ + ".json"
-            with open(filename, 'w') as file:
-                file.write(cls.to_json_string([obj.to_dictionary() for obj in list_objs]))
+        with open(filename, 'w') as file:
+            json.dump([obj.to_dictionary() for obj in list_objs], file)
 
     def to_dictionary(self):
         """Returns the dictionary representation of a Rectangle."""
-        return {'id': self.id,
-                'width': self.width,
-                'height': self.height,
-                'x': self.x,
-                'y': self.y
-                }
-
+        return {'id': self.id, 'width': self.width, 'height': self.height, 'x': self.x, 'y': self.y}
--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,31 +1,30 @@
 #!/usr/bin/python3
+"""Defines a Square class."""
 from models.rectangle import Rectangle
 
+
 class Square(Rectangle):
-    """define square class"""
+    """Define Square class"""
+
     def __init__(self, size, x=0, y=0, id=None):
-        """initalize square class
+        """Initialize Square instance.
+
         Args:
-            size: is the size of square
-            x: is x coordainte of square
-            y: is y coordiante of square
-            id: is id of square
+            size (int): The size of the square.
+            x (int): The x-coordinate of the square.
+            y (int): The y-coordinate of the square.
+            id (int): The identity of the square.
         """
-        
         super().__init__(size, size, x, y, id)
 
     @property
     def size(self):
-        """Define getter function that get size
-        Return: size"""
+        """Getter function for size attribute."""
         return self.width
 
     @size.setter
     def size(self, value):
-        """Define size setter function that set size of square
-        Args:
-
-            """
+        """Setter function for size attribute."""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value <= 0:
@@ -34,20 +33,19 @@ class Square(Rectangle):
         self.height = value
 
     def __str__(self):
+        """Return string representation of Square instance."""
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.size}"
 
     def update(self, *args, **kwargs):
+        """Update attributes of the Square instance."""
         if args:
-            attr = ['id', 'size', 'x', 'y']
+            attrs = ['id', 'size', 'x', 'y']
             for i, value in enumerate(args):
-                setattr(self, attr[i], value)
+                setattr(self, attrs[i], value)
         else:
-            for k, value in kwargs.items():
-                setattr(self, k, value)
+            for key, value in kwargs.items():
+                setattr(self, key, value)
 
     def to_dictionary(self):
-        return {'id': self.id,
-                'size': self.width,
-                'x': self.x,
-                'y': self.y
-                }
+        """Return dictionary representation of Square instance."""
+        return {'id': self.id, 'size': self.size, 'x': self.x, 'y': self.y}
