e289bba016f83d2c7ab2405ccd77fb02e7296ae1
mohamedRajab0 <elzaimmohamed@gmail.com>
2024-02-26 10:03:42 -0800

    done

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+import json
 
 """Module-1 Base class"""
 
@@ -12,4 +13,9 @@ class Base:
         else:
             Base.__nb_objects +=1
             self.id = Base.__nb_objects
+    
+    def to_json_string(list_dictionaries):
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
 
---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+from models.rectangle import Rectangle
+
+class Square(Rectangle):
+    """define square class"""
+    def __init__(self, size, x=0, y=0, id=None):
+        """initalize square class
+        Args:
+            size: is the size of square
+            x: is x coordainte of square
+            y: is y coordiante of square
+            id: is id of square
+        """
+        
+        super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.width = value
+        self.height = value
+
+    def __str__(self):
+        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.size}"
+
+    def update(self, *args, **kwargs):
+        if args:
+            attr = ['id', 'size', 'x', 'y']
+            for i, value in enumerate(args):
+                setattr(self, attr[i], value)
+        else:
+            for k, value in kwargs.items():
+                setattr(self, k, value)
+
+    def to_dictionary(self):
+        return {'id': self.id,
+                'size': self.width,
+                'x': self.x,
+                'y': self.y
+                }
