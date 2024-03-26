66d941d3d1ce10e9b607df27b0616046f85090b2
mohamedRajab0 <elzimmohamed@gmail.com>
2024-03-26 03:47:11 -0700

    upload to check

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,4 +1,4 @@
 -- script that displays the number of records with id = 89 in the
 -- table first_table
-SELECT COUNT(*)  FROM frist_table WHERE id = 89;
+SELECT COUNT(*)  FROM first_table WHERE id = 89;
 
