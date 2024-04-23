8a0b0b4af1373f04697df7f9c7099bf1cd69b17c
mohamedRajab0 <elzimmohamed@gmail.com>
2024-04-23 20:18:24 +0200

    fix style

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-class Rectangle{
+class Rectangle {
   constructo (w, h) {
     this.width = w;
     this.height = h;
--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,12 +1,11 @@
 #!/usr/bin/node
-class Rectangle{
+class Rectangle {
   constructo (w, h) {
-    if ((w > 0) && (h > 0)){
+    if ((w > 0) && (h > 0)) {
       this.width = w;
       this.height = h;
     }
   }
 }
-  
+ 
 module.export = Rectangle;
-  
