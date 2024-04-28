5bb99c0da03126fb742bdbd033530d769ed4d7c0
mohamedRajab0 <elzimmohamed@gmail.com>
2024-04-28 09:02:06 -0700

    fix 7-model file

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -12,5 +12,4 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
     session = Session(engine)
     for state in session.query(State).order_by(State.id).all():
-        print(f"{state.id}: {state.name}")
-    session.close()
+        print(state.id, state.name, sep=": ")
