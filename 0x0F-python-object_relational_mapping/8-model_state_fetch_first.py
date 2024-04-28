dabb216fa6299d133b253014e612b9881b7e54b7
mohamedRajab0 <elzaimmohamed@gmail.com>
2024-04-28 19:08:03 +0300

    fix session

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -10,7 +10,7 @@ from sqlalchmey import Session
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
-    session = Session((engine)
+    session = Session(engine)
     frist = session.query(State).order_by(State.id).frist()
     if frist:
         print(f"{frist.id}: {frist.name}")
