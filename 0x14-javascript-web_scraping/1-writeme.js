876738309c2e2e90679ed42236efd97a21229b8d
mohamedRajab0 <elzaimohamed@gmail.com>
2024-06-11 18:58:13 +0300

    task2

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,7 +1,9 @@
 #!/usr/bin/node
 const fs = require('fs');
-fs.writeFile(process.argv[2], process.argv[3], 'utf8', (err) =>{
-    if (err){
-        console.log(err);
-    }
+const path = process.argv[2];
+const string = process.argv[3];
+fs.writeFile(path, string, function (err) {
+  if (err) {
+    console.log(err);
+  }
 });
